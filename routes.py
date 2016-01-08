42efc256657c5e241896560ab28f5e9ebe6e4144
rmwu <rmwu@mit.edu>
2016-01-08 11:31:39 -0500

    6005 port
    avoid conflicts

--- routes.py
+++ routes.py
@@ -175,4 +175,4 @@ def index():
 # Start Application
 #----------------------------------------------------------
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=6005, debug=True)
