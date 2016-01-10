b08dd7e69c0061cb1f0ec937bbeb3a1f91dfb927
Rachel Wu <rmwu@mit.edu>
2016-01-10 00:45:37 -0500

    not-really-working
    :D

--- static/js/home.js
+++ static/js/home.js
@@ -1,28 +1,24 @@
-$(document).ready(function(){
-
+$(document).ready(function()
+{
     // upon clicking the last li in the list, a new li will be 
     // formed right above it (so last li remains last)
-	var last_item = $(".list li:last-child");
-	last_item.on("click", function(){
-		last_item.before('<li><input type="text"></li>');
-	});
-    
+    $(".list").on("click", ".addRow", function()
+    {
+        $(".addRow").before('<div class="attributes"><div class="attribute"><div class="key"><input type="text"></div><div class = "value"><input type="text"></div></div></div>');
+    });
+
     // upon clicking the "V" shaped icon, the list of attributes
     // will slide down and appear
     $(".list").on("click", "img", function()
     {
-        $(this).next("ul").slideDown(500);
+        $(this).find("attribute").slideDown(500);
+        $(this).find("attribute").attr("display","block");
     });
-    
-    $(".list").on("click", "li", function()
-    {
-        $(this).next("ul").slideDown(500);
-    });
-    
+
     // upon the "title" li's losing focus, the list of attributes
     // will slide back up and hide
-    $(".list").on("mouseleave", "img", function()
+    $(".list").on("blur", "listItem", function()
     {
-        $(this).next("ul").slideUp(500);
+        $(this).find("attribute").slideUp(500);
     });
 });
