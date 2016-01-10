c7f15b66f5feaecc15cfc7d10ecae3d1f5ddff9c
Rachel Wu <rmwu@mit.edu>
2016-01-09 20:34:16 -0500

    2nd li yo-yo up and down :)
    it appears.. and doesn’t disappear atm or push things away ><

--- static/js/home.js
+++ static/js/home.js
@@ -1,7 +1,28 @@
 $(document).ready(function(){
 
+    // upon clicking the last li in the list, a new li will be 
+    // formed right above it (so last li remains last)
 	var last_item = $(".list li:last-child");
-	last_item.click(function(){
+	last_item.on("click", function(){
 		last_item.before('<li><input type="text"></li>');
 	});
+    
+    // upon clicking the "V" shaped icon, the list of attributes
+    // will slide down and appear
+    $(".list").on("click", "img", function()
+    {
+        $(this).next("ul").slideDown(500);
+    });
+    
+    $(".list").on("click", "li", function()
+    {
+        $(this).next("ul").slideDown(500);
+    });
+    
+    // upon the "title" li's losing focus, the list of attributes
+    // will slide back up and hide
+    $(".list").on("mouseleave", "img", function()
+    {
+        $(this).next("ul").slideUp(500);
+    });
 });
