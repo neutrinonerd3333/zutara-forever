71aefb50680cbf4e9422762ef64f67c8e7f31537
Tony Zhang <txz@mit.edu>
2016-01-14 22:42:01 +0800

    Added support for HTTP status code 410: Gone

--- routes.py
+++ routes.py
@@ -232,6 +232,11 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(410)
+def page_not_found(e):
+    return render_template('410.html'), 410
+
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
