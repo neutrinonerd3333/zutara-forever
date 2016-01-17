2622e7c1116aa54c358196a805d89af14d5f5993
Rachel Wu <rmwu@mit.edu>
2016-01-16 22:43:08 -0800

    permission 1/2 way backup

--- static/js/mylists.js
+++ static/js/mylists.js
@@ -1,3 +1,4 @@
+var listid = 0
 $(document).ready(function()
 {
     
@@ -11,12 +12,10 @@ $(document).ready(function()
     $(".listWrapper").on("mouseleave", hideToolbox);
     
     // copy url to clipboard upon clicking in it
-    $(".toolbox").on("click", "input", function(){
-        $(this).select();
-        document.execCommand("copy");
-        alert("Link copied to clipboard!");
-    });
+    $(".toolbox").on("click", "input", useButtons);
     
+    // focusout saves permissions and checks for delete
+    $(".toolbox").on("focusout", "input", saveSettings);
     
 });
 function previewLink()
@@ -35,30 +34,26 @@ function showToolbox()
     tools.show();
     
     var url = $(this).first("input[type='hidden']").val();
-    var listid = url.slice(9);
+    // cut out "/preview/"
+    listid = url.slice(9);
     var perm = $(tools).find("input[type='hidden']").val();
     // wipe out hidden input so don't have to keep repeating script
     if(perm != undefined)
     {
-        console.log(perm);
-
-        var permBox = $(tools).find(".permissions");
         if(perm==="own")
         {
-            $(tools).css("height","8em");
-            $(permBox).css("height","6em");
-            $(permBox).html("You are the owner of this list.");
+            $(tools).html('<div class="icon-container"> <div class="icon-share icon"></div> <div class="icon-view icon"></div> <div class="icon-edit icon"></div> <div class="icon-link icon"></div> <div class="icon-trash icon"></div> </div> <div class="permissions"> <div class="line">You are the owner of this list.</div> <input class="editors" id="view" type="text"> <input class="editors" id="edit" type="text"> <input class="editors" id="url" type="text"> <input class="editors" id="delete" type="text" placeholder="Type &quot;delete&quot; to delete list."> </div>');
+            $(tools).css("height","11em");
         }
         else if(perm==="edit")
         {
-            $(permBox).html("You may view and edit this list.");
-            $(tools).css("height","8em");
-            $(permBox).css("height","6em");
-            $(permBox).html("You are the owner of this list.<br>Add editor.<br>Add viewer.<br>Delete list.");
+            $(tools).html('<div class="icon-container"> <div class="icon-share icon"></div> <div class="icon-link-2 icon"></div> </div> <div class="permissions"> <div class="line">You may edit and view this list.</div> <input class="editors" id="url" type="text"> </div>');
+            $(tools).css("height","5em");
         }
         else if(perm==="view")
         {
-            $(permBox).html("You may view this list.");
+            $(tools).html('<div class="icon-container"> <div class="icon-share icon"></div> <div class="icon-link-2 icon"></div> </div> <div class="permissions"> <div class="line">You may view this list.</div> <input class="editors" id="url" type="text"> </div>');
+            $(tools).css("height","5em");
         }
     }
 }
@@ -67,10 +62,36 @@ function hideToolbox()
     var tools = $(this).find(".toolbox");
     tools.hide();
 }
+function useButtons()
+{
+    // url gets copied to clipboard every time
+    if($(this).attr("id")==="url")
+    {
+        $(this).select();
+        document.execCommand("copy");
+        alert("Link copied to clipboard!");
+    }
+}
+function saveSettings()
+{
+    if($(this).attr("id")==="delete")
+    {
+        if($(this).val()==="delete")
+        {
+            deleteList(listid);
+        }
+    }
+    else if($(this).attr("id")==="view")
+    {
+    }
+    else if($(this).attr("id")==="edit")
+    {
+    }
+}
 function deleteList(listid)
 {
     // verify once more that the user is indeed the owner of the list
-    var isOwner = False;
+    var isOwner = false;
     $.ajax({
         url: "/api/getpermissions",
         method: 'POST',
@@ -78,18 +99,24 @@ function deleteList(listid)
             listid: listid,
         },
         success: function(data, status, jqxhr){
-            isOwner = True;
+            isOwner = true;
         }
     })
-    $.ajax({
-        url: "/api/deletelist",
-        method: 'POST',
-        data: {
-            listid: listid,
-        },
-        success: function(data, status, jqxhr){
-            alert("You have deleted list " + listid);
-            location.reload(true); // reload from server not cache
+    if(isOwner)
+    {
+        if (confirm("Are you sure you want to permanently delete your list?"))
+        {
+            $.ajax({
+                url: "/api/deletelist",
+                method: 'POST',
+                data: {
+                    listid: listid,
+                },
+                success: function(data, status, jqxhr){
+                    alert("You have deleted list " + listid);
+                    location.reload(true); // reload from server not cache
+                }
+            });
         }
-    })
+    }
 }
